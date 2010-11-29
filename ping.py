8406025e50fcff9ab62aa1351a7226adcb717b05
Will Abson <will.abson@gmail.com>
2010-11-29 13:17:30 +0000

    Add initial ping script
    git-svn-id: https://share-import-export.googlecode.com/svn/share-import-export@12 537a4a01-4f98-3f0e-11bd-e6e8c48d5aec

---
+++ ping.py
@@ -0,0 +1,53 @@
+import json, alfresco, sys, re, getopt, os, xml.etree, socket
+
+# HTTP debugging flag
+global _debug
+
+def usage():
+    print "Usage: python ping.py [--username=username] [--password=username] [--url=username] [--timeout=timeout] [-d]"
+
+def main(argv):
+
+    username = "admin"
+    password = "admin"
+    url = "http://localhost:8080/share"
+    _debug = 0
+    # timeout in seconds
+    timeout = 10
+    
+    try:
+        opts, args = getopt.getopt(argv, "hdu:p:U:", ["help", "username=", "password=", "url="])
+    except getopt.GetoptError, e:
+        usage()
+        sys.exit(1)
+    
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            usage()
+            sys.exit()
+        elif opt == '-d':
+            _debug = 1
+        elif opt in ("-u", "--username"):
+            username = arg
+        elif opt in ("-p", "--password"):
+            password = arg
+        elif opt in ("-U", "--url"):
+            url = arg
+        elif opt == '--timeout':
+            timeout = arg
+    
+    socket.setdefaulttimeout(timeout)
+    
+    sc = alfresco.ShareClient(url, debug=_debug)
+    if not filename == "-":
+        print "Log in (%s)" % (username)
+    loginres = sc.doLogin(username, password)
+    if not loginres['success']:
+        print "Could not log in using specified credentials"
+        sys.exit(1)
+     if not filename == "-":
+         print "Log out (%s)" % (username)
+     sc.doLogout()
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
